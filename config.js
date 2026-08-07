947b00c002298def3141a953da2291afcec09987
Olga Nova <contactnova23@gmail.com>
2026-08-07 16:48:51 +0200

    Rename supabase variable to supabaseClient

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://tfoqdoyqlsgmvrrsmwqs.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable__PfAaext-LlwEptiIXpqrQ_BO9Kv4nS';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
