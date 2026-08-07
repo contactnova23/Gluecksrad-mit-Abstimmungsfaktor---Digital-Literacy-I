6c18c2465b2624b8c035e461d33b4d2621f4aafb
Olga Nova <contactnova23@gmail.com>
2026-08-07 15:58:59 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://your-project-ref.supabase.co';
-const SUPABASE_ANON_KEY = 'your-anon-key';
+const SUPABASE_URL = 'https://tfoqdoyqlsgmvrrsmwqs.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable__PfAaext-LlwEptiIXpqrQ_BO9Kv4nS';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
